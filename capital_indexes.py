375d35a4ae368d3c1ec67a9b6aa848c3c7e205aa
Leah T <109878405+LeahTajon@users.noreply.github.com>
2022-08-04 22:37:48 +1000

    Update capital_indexes.py

--- capital_indexes.py
+++ capital_indexes.py
@@ -1,3 +1,17 @@
+"""
+File: capital_indexes.py
+Author: Leah Tajon
+
+Challenge: Capital indexes
+
+Assignment: Write a function named capital_indexes. The function takes a 
+single parameter, which is a string. Your function should return a list of
+all the indexes in the string that have capital letters.
+
+For example, calling capital_indexes("HeLlO") should return the list 
+[0, 2, 4]
+"""
+
 def capital_indexes(letters):
     list_1 = []
     
@@ -7,5 +21,5 @@ def capital_indexes(letters):
     
     return list_1
     
-result = capital_indexes("TEsT")
+result = capital_indexes("HeLlO")
 print(result)
