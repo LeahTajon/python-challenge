fe8bf45b529906ca244a1a046f7232d0639bf558
Leah T <109878405+LeahTajon@users.noreply.github.com>
2022-08-07 22:22:09 +1000

    Add files via upload

---
+++ double_letters.py
@@ -0,0 +1,29 @@
+"""
+File: double_letters.py
+Author: Leah Tajon
+
+Challenge #6: Double letters
+
+Assignment: The goal of this challenge is to analyze
+a string if it contains two of the same letter in a row.
+For example, the string "hello" has 'l' twice in a row, 
+while the string "nono" does not have two identical letters
+in a row.
+
+Define a function named double_letters that takes a single
+parameter. The parameter is a string. Your function must 
+return True if there are two identical letters in a row in the
+string, and False otherwise.
+"""
+
+def double_letters(word):
+    for i in range(len(word)-1):
+        if word[i] == word[i+1]:
+            return True
+    return False
+
+ans1 = double_letters("nono")
+print(ans1)
+ans2 = double_letters("hello")
+print(ans2)
+
